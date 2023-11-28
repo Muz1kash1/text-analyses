ac9d607a75e7b531cb3a9e625be6379fe8ec8cff
FoxRLY <nevik0016@gmail.com>
2023-11-28 17:40:44 +0300

    Remove usage of legacy id system
    Убрано использование неудобной системы старых id, исправлен баг с изменением старых эталонов, улучшена читаемость кода и его оптимизация

--- text_similarity_engine.py
+++ text_similarity_engine.py
@@ -6,9 +6,11 @@ import re
 from nltk import sent_tokenize, word_tokenize, pos_tag
 import pika
 import tempfile
-from database import DatabaseLegacy
+from database import Database, ReferenceSample
 from dotenv import load_dotenv
 import os
+import uuid
+
 
 def pymorphy2_311_hotfix():
     from inspect import getfullargspec
@@ -20,9 +22,10 @@ def pymorphy2_311_hotfix():
         args = getfullargspec(klass.__init__).args
         return sorted(args[1:])
 
-    setattr(BaseAnalyzerUnit, '_get_param_names', _get_param_names_311)
+    setattr(BaseAnalyzerUnit, "_get_param_names", _get_param_names_311)
 
-def normalize_word(word):
+
+def normalize_word(word: str) -> str:
     """
     Нормализует слово, приводя его к нормальной (базовой) форме с использованием морфологического анализатора.
 
@@ -46,7 +49,7 @@ def normalize_word(word):
     return parsed_word[2]
 
 
-def split_text_into_fragments(text, max_series=5):
+def split_text_into_fragments(text: str, max_series=5):
     """
     Разделяет текст на фрагменты, используя предложения как базовые единицы.
 
@@ -73,19 +76,19 @@ def split_text_into_fragments(text, max_series=5):
     # Создание фрагментов
     while series:
         index = len(fragments) * max_series
-        fragments.append(sentences[index:index + max_series])
+        fragments.append(sentences[index : index + max_series])
         series = (len(sentences) - len(fragments) * max_series) // max_series
 
     # Обработка оставшихся предложений после создания максимального числа фрагментов
     last_index = len(sentences) // max_series
     if len(sentences) % max_series:
-        fragments.append(sentences[last_index * max_series:])
+        fragments.append(sentences[last_index * max_series :])
 
     # Возвращение списка фрагментов
     return fragments
 
 
-def extract_first_signs(fragment):
+def extract_first_signs(fragment: list[str]) -> list[list[str]]:
     """
     Извлекает признаки первого уровня из фрагмента текста.
 
@@ -108,10 +111,12 @@ def extract_first_signs(fragment):
         word_list = word_tokenize(sentence)
 
         # Очистка слов от символов, не являющихся кириллическими буквами
-        cleaned_word_list = [re.sub('[^а-яё]', '', word.lower()) for word in word_list if len(word) > 2]
+        cleaned_word_list = [
+            re.sub("[^а-яё]", "", word.lower()) for word in word_list if len(word) > 2
+        ]
 
         # Удаление пустых строк из списка слов
-        cleaned_word_list = list(filter(('').__ne__, cleaned_word_list))
+        cleaned_word_list = list(filter(("").__ne__, cleaned_word_list))
 
         # Нормализация слов и добавление их в список признаков
         signs.append(list(set(map(normalize_word, cleaned_word_list))))
@@ -120,7 +125,7 @@ def extract_first_signs(fragment):
     return signs
 
 
-def extract_second_signs(signs_list):
+def extract_second_signs(signs_list: list[list[str]]) -> list[list[str]]:
     """
     Извлекает признаки второго уровня из списка признаков первого уровня.
 
@@ -134,10 +139,13 @@ def extract_second_signs(signs_list):
     >>> extract_second_signs([['это', 'предложение'], ['и', 'это', 'еще', 'одно', 'предложение']])
     [['предложение'], ['предложение']]
     """
+
     # Внутренняя функция для получения существительных и глаголов из набора признаков
     def get_noun_verb_signs(sign_set):
-        parts_of_speech = pos_tag(sign_set, lang='rus')
-        noun_verb_signs = [part[0] for part in parts_of_speech if part[1][0] in ['S', 'V']]
+        parts_of_speech = pos_tag(sign_set, lang="rus")
+        noun_verb_signs = [
+            part[0] for part in parts_of_speech if part[1][0] in ["S", "V"]
+        ]
         return noun_verb_signs
 
     # Инициализация пустого списка для хранения признаков второго уровня
@@ -159,7 +167,7 @@ def extract_second_signs(signs_list):
     return signs
 
 
-def extract_third_signs(signs_list):
+def extract_third_signs(signs_list: list[list[str]]) -> list[list[str]]:
     """
     Извлекает признаки третьего уровня из списка признаков второго уровня.
 
@@ -173,10 +181,11 @@ def extract_third_signs(signs_list):
     >>> extract_third_signs([['предложение'], ['предложение']])
     [['предложение']]
     """
+
     # Внутренняя функция для получения существительных из набора признаков
     def get_nouns(sign_set):
-        parts_of_speech = pos_tag(sign_set, lang='rus')
-        noun_signs = [part[0] for part in parts_of_speech if part[1][0] == 'S']
+        parts_of_speech = pos_tag(sign_set, lang="rus")
+        noun_signs = [part[0] for part in parts_of_speech if part[1][0] == "S"]
         return noun_signs
 
     # Инициализация пустого списка для хранения признаков третьего уровня
@@ -198,7 +207,7 @@ def extract_third_signs(signs_list):
     return signs
 
 
-def generate_signatures(fragment):
+def generate_signatures(fragment: list[str]) -> list[list[list[str]]]:
     """
     Генерирует сигнатуры для фрагмента текста, используя функции извлечения признаков разных уровней.
 
@@ -296,7 +305,13 @@ def update_dictionary(sign_list, etalon_fragment, fragment_id, dictionary):
     {'123456_001': [2, 3, 2, 3]}
     """
     # Получение списка весов для каждого признака относительно эталонного фрагмента
-    weights_list = [sorted([compare_signatures(sign, etalon_sign) for etalon_sign in etalon_fragment], key=lambda x: x[0] / x[1])[-1] for sign in sign_list]
+    weights_list = [
+        sorted(
+            [compare_signatures(sign, etalon_sign) for etalon_sign in etalon_fragment],
+            key=lambda x: x[0] / x[1],
+        )[-1]
+        for sign in sign_list
+    ]
 
     # Обновление словаря весов для данного фрагмента текста
     dictionary[fragment_id] = sorted(weights_list, key=lambda x: x[0] / x[1])[-1]
@@ -305,7 +320,14 @@ def update_dictionary(sign_list, etalon_fragment, fragment_id, dictionary):
     return dictionary
 
 
-def main_check(input_filename, db, similarity_border=0.1, max_series=5, id_legend=[6, 3]):
+class InputData:
+    def __init__(self, id: uuid.UUID, text: str, label: str):
+        self.id = id
+        self.text = text
+        self.label = label
+
+
+def main_check(input_filename, db: Database, similarity_border=0.1, max_series=5):
     """
     Основная функция для проверки схожести фрагментов текста с эталонами и обновления базы данных.
 
@@ -326,121 +348,104 @@ def main_check(input_filename, db, similarity_border=0.1, max_series=5, id_legen
     ({'123456': 'Это текст'}, {'123456_001': [['Это предложение.'], 0.8]})
     """
     # Чтение входных данных из файла JSON
-    texts_data = {}
-    with open(input_filename, "r", encoding='utf-8') as read_file:
+    texts_data: list[InputData] = []
+    with open(input_filename, "r", encoding="utf-8") as read_file:
         json_data = json.load(read_file)
         for item in json_data:
-            texts_data[item['id']] = [item['text'], item['label']]
+            texts_data.append(InputData(uuid.uuid4(), item["text"], item["label"]))
 
     # Чтение данных эталонов из файла JSON
     etalons_data = db.get_reference_samples()
-    # etalons_data = {}
-    # with open(db_filename, "r", encoding='utf-8') as read_file:
-    #     json_data = json.load(read_file)
-    #     for item in json_data:
-    #         order_1 = [part.split(',') for part in item['order1'].split(';')]
-    #         order_2 = [part.split(',') for part in item['order2'].split(';')]
-    #         order_3 = [part.split(',') for part in item['order3'].split(';')]
-    #         if all([order_1, order_2, order_3]):
-    #             etalons_data[item['id']] = [order_1, order_2, order_3, item['weight']]
 
     # Инициализация словарей и списков для хранения данных
     dict_1, dict_2, dict_3 = {}, {}, {}
-    new_etalon_weights, sign_dict = {}, {}
+    new_etalon_weights = {}
+    sign_dict: dict[str, ReferenceSample] = {}
 
     # Обработка текстовых данных
-    for text_id in texts_data.keys():
-        fragments = split_text_into_fragments(texts_data[text_id][0], max_series)
+    # Получаем отдельные тексты из набора текстов
+    for text_sample in texts_data:
+        # Делим текст на фрагменты
+        fragments = split_text_into_fragments(text_sample.text, max_series)
+        # Проходимся по фрагментам
+        for i, fragment in enumerate(fragments):
+            # Составляем внутренний id в таком виде: uuid_part
+            fragment_id = "_".join([str(text_sample.id), str(i)])
+            # Создаем объект новой обработанной части текста
+            new_reference_sample = ReferenceSample(
+                id=text_sample.id, part=i, order1=[], order2=[], order3=[], weight=0
+            )
+            # Заполняем его признаки
+            (
+                new_reference_sample.order1,
+                new_reference_sample.order2,
+                new_reference_sample.order3,
+            ) = generate_signatures(fragment)
+            # Сохраняем его в словаре обработанных частей текстов по fragment_id
+            sign_dict[fragment_id] = new_reference_sample
+            # Если текст является эталонным, то сохраняем его в отдельном словаре вида uuid_part: label и пропускаем
+            if text_sample.label != "?":
+                new_etalon_weights[fragment_id] = int(text_sample.label)
+                continue
+            #
+            for etalon in etalons_data:
+                # Проверка, не является ли текущий текст эталоном < БОЛЬШОЙ ВОПРОС
 
-        for etalon_id in etalons_data.keys():
-            # Проверка, не является ли текущий текст эталоном
-            if texts_data[text_id][1] != '?':
-                new_etalon_weights[etalon_id] = int(texts_data[text_id][1])
-
-            # Обработка фрагментов текущего текста
-            if get_text_id(etalon_id, id_legend[0]) != text_id:
-                for i, fragment in enumerate(fragments):
-                    fragment_id = text_id + '0' * (id_legend[1] - len(str(i))) + str(i)
-                    sign_dict[fragment_id] = generate_signatures(fragment)
-                    dict_1 = update_dictionary(sign_dict[fragment_id][0], etalons_data[etalon_id][0], fragment_id, dict_1)
-                    dict_2 = update_dictionary(sign_dict[fragment_id][1], etalons_data[etalon_id][1], fragment_id, dict_2)
-                    dict_3 = update_dictionary(sign_dict[fragment_id][2], etalons_data[etalon_id][2], fragment_id, dict_3)
-            else:
-                break
+                # Обработка фрагментов текущего текста
+                if etalon.id == text_sample.id:
+                    break
+                dict_1 = update_dictionary(
+                    new_reference_sample.order1, etalon.order1, fragment_id, dict_1
+                )
+                dict_2 = update_dictionary(
+                    new_reference_sample.order2, etalon.order2, fragment_id, dict_2
+                )
+                dict_3 = update_dictionary(
+                    new_reference_sample.order3, etalon.order3, fragment_id, dict_3
+                )
 
     # Инициализация словаря весов для фрагментов текста
-    fragment_weights = {}
-    target_fragment_ids = []
-    new_etalons_data = []
+    target_fragments: list[ReferenceSample] = []
 
     # Расчет весов для фрагментов текста
     for fragment_id in dict_1.keys():
+        # Если фрагмент является эталонным, то берем его эталонное значение
         if fragment_id in new_etalon_weights:
-            fragment_weights[fragment_id] = new_etalon_weights[fragment_id]
+            sign_dict[fragment_id].weight = new_etalon_weights[fragment_id]
+        # Иначе расчитываем по весам, полученным при сравнении с другими эталонными значениями
         else:
-            fragment_weights[fragment_id] = (3 * dict_1[fragment_id][0] / dict_1[fragment_id][1] +
-                                             2 * dict_2[fragment_id][0] / dict_2[fragment_id][1] +
-                                             dict_3[fragment_id][0] / dict_3[fragment_id][1]) / 6
+            sign_dict[fragment_id].weight = (
+                3 * dict_1[fragment_id][0] / dict_1[fragment_id][1]
+                + 2 * dict_2[fragment_id][0] / dict_2[fragment_id][1]
+                + dict_3[fragment_id][0] / dict_3[fragment_id][1]
+            ) / 6
 
         # Проверка, является ли фрагмент текста целевым
-        if fragment_weights[fragment_id] > similarity_border:
-            target_fragment_ids.append(fragment_id)
-
-        # Создание строк для представления признаков в виде текста
-        str_sig_1 = ';'.join([','.join(sign_set) for sign_set in sign_dict[fragment_id][0]])
-        str_sig_2 = ';'.join([','.join(sign_set) for sign_set in sign_dict[fragment_id][1]])
-        str_sig_3 = ';'.join([','.join(sign_set) for sign_set in sign_dict[fragment_id][2]])
-
-        # Создание новых эталонных данных для целевых фрагментов
-        if fragment_weights[fragment_id]:
-            new_etalons_data.append({'id': fragment_id, 'order1': str_sig_1, 'order2': str_sig_2,
-                                     'order3': str_sig_3, 'weight': fragment_weights[fragment_id]})
+        if sign_dict[fragment_id].weight > similarity_border:
+            target_fragments.append(sign_dict[fragment_id])
 
     # Обновление данных базы данных
-    current_data = []
-    data = db.get_reference_samples_scuffed()
-    for item in data:
-        if item["id"] in new_etalon_weights:
-            if new_etalon_weights[item["id"]]:
-                current_data.append({'id': item['id'], 'order1': item['order1'], 'order2': item['order2'],
-                                     'order3': item['order3'], 'weight': new_etalon_weights[item['id']]})
-            else:
-                current_data.append(item)
-    # current_data = []
-    # with open(db_filename, "r", encoding='utf-8') as file:
-    #     data = json.load(file)
-    #     for item in data:
-    #         if item['id'] in new_etalon_weights:
-    #             if new_etalon_weights[item['id']]:
-    #                 current_data.append({'id': item['id'], 'order1': item['order1'], 'order2': item['order2'],
-    #                                      'order3': item['order3'], 'weight': new_etalon_weights[item['id']]})
-    #         else:
-    #             current_data.append(item)
-
-    # Добавление новых эталонных данных
-    for new_etalon in new_etalons_data:
-        if new_etalon['id'] not in [existing_data['id'] for existing_data in current_data]:
-            current_data.append(new_etalon)
+    etalons_data.extend(sign_dict.values())
 
     # Запись обновленных данных в файл базы данных
-    db.insert_new_samples(current_data)
-    # with open(db_filename, "w", encoding='utf-8') as output_file:
-    #     json.dump(current_data, output_file, ensure_ascii=False)
+    db.insert_new_samples(etalons_data)
 
-    # Формирование результата - текстов и целевых фрагментов
-    target_texts = {}
-    target_fragments = {}
+    # Формирование результата - целевых фрагментов и содержащих их текстов
 
-    for target_fragment_id in target_fragment_ids:
-        target_text_id = get_text_id(target_fragment_id[:id_legend[0]])
-        if target_text_id not in target_texts.keys():
-            target_texts[target_text_id] = texts_data[target_text_id][0]
+    # Получение id текстов, содержащих целевые фрагменты
+    target_text_ids = set([fragment.id for fragment in target_fragments])
+    # Получение текстов в виде словаря вида - uuid: текст
+    result_target_texts = dict(
+        [(text.id, text.text) for text in texts_data if text.id in target_text_ids]
+    )
+    result_target_fragments = dict(
+        [
+            (fragment.id, [fragment.part, fragment.weight])
+            for fragment in target_fragments
+        ]
+    )
 
-        fragments = split_text_into_fragments(target_texts[target_text_id], max_series)
-        target_fragments[target_fragment_id] = [fragments[int(target_fragment_id[-id_legend[1]:])],
-                                                fragment_weights[target_fragment_id]]
-
-    return target_texts, target_fragments
+    return result_target_texts, result_target_fragments
 
 
 if __name__ == "__main__":
@@ -448,31 +453,34 @@ if __name__ == "__main__":
     load_dotenv()
 
     # Получить значения переменных окружения
-    db_user = os.getenv("DB_USER")
-    db_password = os.getenv("DB_PASSWORD")
-    db_name = os.getenv("DB_NAME")
-    db_host = os.getenv("DB_HOST")
-    db_port = int(os.getenv("DB_PORT"))
-    rabbit_host = os.getenv("MQ_HOST_NAME")
+    db_user = val if (val := os.getenv("DB_USER")) is not None else "postgres"
+    db_password = val if (val := os.getenv("DB_PASSWORD")) is not None else "password"
+    db_name = val if (val := os.getenv("DB_NAME")) is not None else "postgres"
+    db_host = val if (val := os.getenv("DB_HOST")) is not None else "postgres"
+    db_port = int(val) if (val := os.getenv("DB_PORT")) is not None else 5432
+    rabbit_host = val if (val := os.getenv("MQ_HOST_NAME")) is not None else "I dunno"
 
     similarity_border = float(sys.argv[1])
     pymorphy2_311_hotfix()
-    
-    db = DatabaseLegacy(db_user, db_password, db_name, db_host, db_port)
-    #db.load_json_data("db.json")
 
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbit_host,heartbeat=900))
+    db = Database(db_user, db_password, db_name, db_host, db_port)
+    # db.load_json_data("db.json")
+
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host=rabbit_host, heartbeat=900)
+    )
     channel = connection.channel()
-    queue = channel.queue_declare('texts_analysis')
+    queue = channel.queue_declare("texts_analysis")
     queue_name = queue.method.queue
 
     def callback(ch, method, properties, body):
         payload = json.loads(body.decode())
 
-        with tempfile.NamedTemporaryFile(mode='w+', delete=False, encoding='utf-8') as temp_file:
+        with tempfile.NamedTemporaryFile(
+            mode="w+", delete=False, encoding="utf-8"
+        ) as temp_file:
             json.dump(payload, temp_file, ensure_ascii=False)
             temp_file_name = temp_file.name
-        
 
         # Прямо передаем строку JSON в функцию main_check
         _, target_fragments = main_check(temp_file_name, db, similarity_border)
@@ -481,4 +489,3 @@ if __name__ == "__main__":
 
     channel.basic_consume(on_message_callback=callback, queue=queue_name)
     channel.start_consuming()
-
